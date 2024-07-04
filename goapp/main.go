08a494cccc9f54f68048bb4f891bd0440aa618b5
Argana Phangquestian <arganaphangquestian@gmail.com>
2024-07-04 12:26:43 +0700

    Initial Commit :fire:

---
+++ goapp/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "OK",
+		})
+	})
+
+	http.ListenAndServe("0.0.0.0:8000", nil)
+}
